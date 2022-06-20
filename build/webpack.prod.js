19ddfdc8cd933efc06978a37cdad83636ec9fae1
guojiongwei <18530633992@163.com>
2022-06-20 11:12:39 +0800

    feat: 添加生成css,js的gzip压缩文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -7,6 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+const CompressionPlugin  = require('compression-webpack-plugin')
 const glob = require('glob')
 
 module.exports = merge(baseConfig, {
@@ -49,6 +50,14 @@ module.exports = merge(baseConfig, {
         standard: [/^ant-/],
       }
     }),
+    new CompressionPlugin({
+      test: /\.(js|css)$/, // 只生成css,js压缩文件
+      filename: '[path][base].gz', // 文件命名
+      algorithm: 'gzip', // 压缩格式，默认是gzip
+      test: /\.(js|css)$/, // 只生成css,js压缩文件
+      threshold: 10240, // 只有大小大于该值的资源会被处理。默认值是 10k
+      minRatio: 0.8 // 压缩率,默认值是 0.8
+    })
   ],
   optimization: {
     minimizer: [
