ea8f97486f639571d1f1475a83939cda5bbf9700
guojiongwei <18530633992@163.com>
2022-06-24 09:25:02 +0800

    feat: 测试react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const root = document.getElementById('root');
 if(root) {
-  createRoot(root).render(<App />)
+  createRoot(root).render(
+    <BrowserRouter>
+      <Link to='/'>首页</Link>
+      <Link to='/user'>我的</Link>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="user" element={<User />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Home() {
+  return <h2>首页</h2>
+}
+
+function User() {
+  return <h2>我的</h2>
 }
