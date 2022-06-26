4ffc49846c013461e245b823a9845de6aa33f90d
郭炯韦 <18530633992@163.com>
2022-06-26 21:35:04 +0800

    feat: 取消提取公共代码

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -73,12 +73,12 @@ module.exports = merge(baseConfig, {
           minSize: 0, // 提取代码体积大于0就提取出来
           priority: 1, // 提取优先级为1
         },
-        commons: { // 提取页面公共代码
-          name: 'commons', // 提取文件命名为commons
-          minChunks: 2, // 只要使用两次就提取出来
-          chunks: 'initial', // 只提取初始化就能获取到的模块，不管异步的
-          minSize: 0, // 提取代码体积大于0就提取出来
-        }
+        // commons: { // 提取页面公共代码
+        //   name: 'commons', // 提取文件命名为commons
+        //   minChunks: 2, // 只要使用两次就提取出来
+        //   chunks: 'initial', // 只提取初始化就能获取到的模块，不管异步的
+        //   minSize: 0, // 提取代码体积大于0就提取出来
+        // }
       }
     }
   },
